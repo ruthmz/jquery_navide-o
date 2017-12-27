cc4fe4366be25296df94e81a4cb6a951f62c53b4
ruthmz <rmatosfer@gmail.com>
2017-12-27 02:22:22 -0500

    corriendo html

--- js/signup.js
+++ js/signup.js
@@ -3,18 +3,18 @@ $(document).ready(function() {
   var $password = $('#Inputpassword');
 
   // eventos a los inputs
-$email.on('input', function(){
+  $email.on('input', function() {
     var PATTERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
     console.log($(this).val());
     PATTERNEMAIL.test(str)
-})
+  })
 
 
-$password.on('input', function(){
-    if($(this).val().length >= 6) {
-        alert('cumple');
-     } else {
-         alert('no cumple');
-     }
-})
+  $password.on('input', function() {
+    if ($(this).val().length >= 6) {
+      alert('cumple');
+    } else {
+      alert('no cumple');
+    }
+  })
 });
